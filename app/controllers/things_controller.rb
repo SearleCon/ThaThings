804059096214b917be0e6816c6c1aa34dc0ecdbb
Searle Consulting <shaun@searleconsulting.co.za>
2012-07-24 11:26:40 +0200

    Settings now working all around (rows per page)

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -3,7 +3,10 @@ class ThingsController < ApplicationController
   # GET /things.json
   def index
     # @things = Thing.all
-    @things = Thing.paginate :page => params[:page], :per_page => 15, :order => 'updated_at DESC'
+    
+    @setting = Setting.find(:all, :conditions => ["user_id=?", current_user.id])
+    @things = Thing.paginate :page => params[:page], :per_page => @setting.first.rowcountperpage, :order => 'updated_at DESC'
+
 
     respond_to do |format|
       format.html # index.html.erb
