cc1a656e0d378d187b2eabe72047f9d632f3b8a6
Searle Consulting <shaun@searleconsulting.co.za>
2012-08-01 17:13:58 +0200

    Checking if accessing from a mobile device

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  
+  # Request from an iPhone or iPod touch? (Mobile Safari user agent)
+  def iphone_user_agent?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+  end
+  
 end
